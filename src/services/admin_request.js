ce9fe1ec8428099e58dc9c74a87f0520d6f4a48d
吴志强 <1076976262@qq.com>
2023-05-16 10:21:15 +0800

    feat: 🎸 修改login，文件目录等

--- src/services/admin_request.js
+++ src/services/admin_request.js
@@ -1,13 +1,12 @@
-
 /**
-* @file 数据接口导出模块.
-* @copyright Duoxi Network .CO .LTD @2011
-*/
+ * @file 数据接口导出模块.
+ * @copyright wzq
+ */
 
 
-import { ADMIN_USER_ADD, ADMIN_USER_UPDATE, ADMIN_USER_DELETE, ADMIN_USER_INFO, ADMIN_USER_LIST, ADMIN_USER_LOGIN, } from '@/services/api' 
-import { request, METHOD } from '@/utils/request' 
- 
+import { ADMIN_USER_ADD, ADMIN_USER_UPDATE, ADMIN_USER_DELETE, ADMIN_USER_INFO, ADMIN_USER_LIST, ADMIN_USER_LOGIN, } from '@/services/api'
+import { request, METHOD } from '@/utils/request'
+
 /**
  * 管理后台用户增加
  
@@ -16,7 +15,7 @@ import { request, METHOD } from '@/utils/request'
  */
 export async function post_admin_user_add(params) {
     return request(ADMIN_USER_ADD, METHOD.POST, params ? params : {}, null)
-} 
+}
 
 /**
  * 管理后台用户修改
@@ -26,7 +25,7 @@ export async function post_admin_user_add(params) {
  */
 export async function post_admin_user_update(params) {
     return request(ADMIN_USER_UPDATE, METHOD.POST, params ? params : {}, null)
-} 
+}
 
 /**
  * 管理后台用户删除
@@ -36,7 +35,7 @@ export async function post_admin_user_update(params) {
  */
 export async function post_admin_user_delete(params) {
     return request(ADMIN_USER_DELETE, METHOD.POST, params ? params : {}, null)
-} 
+}
 
 /**
  * 管理后台用户查询
@@ -46,7 +45,7 @@ export async function post_admin_user_delete(params) {
  */
 export async function post_admin_user_info(params) {
     return request(ADMIN_USER_INFO, METHOD.POST, params ? params : {}, null)
-} 
+}
 
 /**
  * 管理后台用户列表
@@ -56,7 +55,7 @@ export async function post_admin_user_info(params) {
  */
 export async function post_admin_user_list(params) {
     return request(ADMIN_USER_LIST, METHOD.POST, params ? params : {}, null)
-} 
+}
 
 /**
  * 管理后台登录
@@ -66,16 +65,16 @@ export async function post_admin_user_list(params) {
  */
 export async function post_admin_user_login(params) {
     return request(ADMIN_USER_LOGIN, METHOD.POST, params ? params : {}, null)
-} 
+}
 
 
 export default {
-    post_admin_user_add, 
-    post_admin_user_update, 
-    post_admin_user_delete, 
-    post_admin_user_info, 
-    post_admin_user_list, 
-    post_admin_user_login, 
+    post_admin_user_add,
+    post_admin_user_update,
+    post_admin_user_delete,
+    post_admin_user_info,
+    post_admin_user_list,
+    post_admin_user_login,
 }
 
 
@@ -102,4 +101,3 @@ export default {
 // post_admin_user_list({});
 
 // post_admin_user_login({});
-
